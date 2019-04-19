55f3d19e08a66be89c4de629bc02fe9db20688fc
Jonathan on Swift <jonathan.lassen@gmail.com>
2019-04-19 15:26:34 -0400

    final tests

--- src/Features/Features.js
+++ src/Features/Features.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Options from './FeatureItem/FeatureItem.js';
+import Options from '../FeatureItem/FeatureItem.js';
 
 class Features extends Component {
     render()  {
