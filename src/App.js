02de3a93d08fb720b89a81c08aa8db9ae33b9869
Harrison Chen <harrisonchen@harrisons-mbp.lan>
2019-04-19 08:58:58 -0700

    sumary part

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Features from './Features';
+import Summary from './Summary';
 
 
 class App extends Component {
@@ -39,19 +40,7 @@ class App extends Component {
   }
 
   render() {
-    console.log(this.state)
-    const summary = Object.keys(this.state.selected)
-          .map(key => <div className="summary__option" key={key}>
-            <div className="summary__option__label">{key}  </div>
-            <div className="summary__option__value">{this.state.selected[key].name}</div>
-            <div className="summary__option__cost">
-              { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                  .format(this.state.selected[key].cost) }
-            </div>
-        </div>)
-
-    const total = Object.keys(this.state.selected)
-          .reduce((acc, curr) => acc + this.state.selected[curr].cost, 0);    
+      
 
 
     
@@ -68,17 +57,7 @@ class App extends Component {
             <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
             <Features features={this.props.features} selected={this.state.selected} updateFeature={this.updateFeature}/>
           </section>
-          <section className="main__summary">
-            <h3>NEW GREENLEAF 2018</h3>
-            {summary}
-            <div className="summary__total">
-              <div className="summary__total__label">Your Price: </div>
-              <div className="summary__total__value">
-              { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                  .format(total) }
-              </div>
-            </div>
-          </section>
+          <Summary selected={this.state.selected}/>
         </main>
       </div>
     );
